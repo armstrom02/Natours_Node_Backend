cbe01266cee491f9419b8d2ef3c2799fcf1ca5b7
armstrom02 <viveksinghreborn@gmail.com>
2019-09-03 15:53:57 +0530

    error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -11,19 +13,11 @@ const app = express();
 /* -------------------------------------------------------------------------- */
 
 if (process.env.NODE_ENV === 'development ') {
-  // console.log('morgan');
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
-
-/* --------------------------- Static page Access --------------------------- */
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  // console.log('hello from the middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.reqestTime = new Date().toISOString();
@@ -34,13 +28,14 @@ app.use((req, res, next) => {
 /*                                   Routes                                   */
 /* -------------------------------------------------------------------------- */
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//Handling unhandled Routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server'`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
