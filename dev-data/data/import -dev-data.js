28d53ea455992a61b9fa7c783e5b5256504b73a5
armstrom02 <viveksinghreborn@gmail.com>
2019-09-11 18:08:16 +0530

    Modelling : Embedding

--- dev-data/data/import -dev-data.js
+++ dev-data/data/import -dev-data.js
@@ -18,9 +18,7 @@ mongoose
   })
   .then(() => console.log('DB connection successfully'));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATABASE_PASSWORD
 const importData = async () => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -61,7 +62,32 @@ const tourSchema = new mongoose.Schema(
     images: [String],
     createAt: { type: Date, default: Date.now(), select: false },
     startDates: [Date],
-    secretTour: { type: Boolean, default: false }
+    secretTour: { type: Boolean, default: false },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      corrdinate: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        descreiption: String,
+        day: Number
+      }
+    ],
+    guide: Array
   },
   {
     toJSON: { virtuals: true },
@@ -79,6 +105,12 @@ tourSchema.pre('save', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
