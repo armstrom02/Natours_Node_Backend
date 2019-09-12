9f329c0705ec14e806a2ee9768980b483332061d
armstrom02 <viveksinghreborn@gmail.com>
2019-09-12 17:53:53 +0530

    implementing /me

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,5 @@
 const Tour = require('./../models/tourModel');
-const APIfeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 /* -------------------------------------------------------------------------- */
