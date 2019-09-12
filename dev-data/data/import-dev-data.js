0038fb93efbdcf177512e311b0621685ee3d0a3d
armstrom02 <viveksinghreborn@gmail.com>
2019-09-12 18:39:30 +0530

    Adding missing Authentication and authorization

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel');
+const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 dotenv.config({ path: './../../config.env' });
 
@@ -19,11 +21,17 @@ mongoose
   .then(() => console.log('DB connection successfully'));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATABASE_PASSWORD
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('data successfully loaded');
   } catch (error) {
     console.log(error);
@@ -34,6 +42,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('data successfully deleted');
   } catch (error) {
     console.log(error);
