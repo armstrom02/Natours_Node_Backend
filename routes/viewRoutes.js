05e5c89b87570f53453bb65b51ac0de5c2314095
armstrom02 <viveksinghreborn@gmail.com>
2019-09-15 20:21:21 +0530

    Building the Tour Page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,7 @@ const viewController = require('./../controllers/viewController');
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
+// router.get('/tours/:slug', viewController.getTour);
 
 module.exports = router;
