1ce07661b53378100f236066f2dc1e170c030fbb
armstrom02 <viveksinghreborn@gmail.com>
2019-09-19 13:19:12 +0530

    Saving Image Name to Database

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,9 +46,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
-
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(new AppError('This route is not for password update.', 400));
@@ -56,6 +53,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
