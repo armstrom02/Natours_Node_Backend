e0c5c308969e21c5df3afc8272ef2f42b1f1a322
armstrom02 <viveksinghreborn@gmail.com>
2019-09-22 20:59:08 +0530

    config and import script

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('../../models/tourModel');
-const Review = require('../../models/reviewModel');
-const User = require('../../models/userModel');
+const Tour = require('./../../models/tourModel');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 
-dotenv.config({ path: './../../config.env' });
+dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE_LOCAL.replace(
+const DB = process.env.DATABASE_ONLINE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
 );
